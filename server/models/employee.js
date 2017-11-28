8bb86fe42ebc70ac1c3be4a2ca574b721e24e279
ariannaordona <ariannaordona@gmail.com>
2017-11-27 20:02:37 -0800

    employee schema

--- server/models/employee.js
+++ server/models/employee.js
@@ -2,40 +2,66 @@ import mongoose from 'mongoose'
 
 import {modelTypes} from '../../common/constants'
 
-const {Schema} = mongoose
-
-var employeeSchema = new Schema({
+const EmployeeSchema = new Schema({
   _id: {
     type: Number,
     ref: modelTypes.USER
   },
-  lastName: {
-    type: String,
-    trim: true
-  },
   firstName: {
     type: String,
-    trim: true
+    required: true
+  },
+  middleName: {
+    type: String,
+    required: false
+  }
+  lastName: {
+    type: String,
+    required: true
   },
   email: {
     type: String,
+    required: true
+  },
+  disclaimerAgree: {
+    type: Boolean
+  },
+  disclaimerSign: {
+    type: String,
     trim: true
   },
-  donations: [{
-    type: Number,
-    ref: modelTypes.DONATION
-  }],
-  fields: [{
-    meta: {
-      type: String,
-      required: true
-    },
-    value: String
-  }],
   dateReceived: {
     type: Date,
     default: Date.now
-  }
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: String
+    required: true
+  },
+  state: {
+    type: String
+    required: true
+  },
+  phone: {
+    type: Number
+    required: true
+  },
+  position: {
+    type: String
+    required: true
+  },
+  socialSecurity: {
+    type: Number
+    required: true
+  },
 }, {
   id: false
 })
@@ -43,13 +69,21 @@ var employeeSchema = new Schema({
 /**
  * Virtual getters & setters
  */
-employeeSchema.virtual('fullName').get(function() {
+EmployeeSchema.virtual('fullName').get(function() {
   var fullName = this.firstName ? this.firstName + ' ' : ''
   fullName += this.middleName ? this.middleName + ' ' : ''
   fullName += this.lastName ? this.lastName : ''
   return fullName
 })
 
-employeeSchema.set('toJSON', {virtuals: true})
+EmployeeSchema.virtual('fullAddress').get(function() {
+  var fullAddress = this.address ? this.address + ' ' : ''
+  fullAddress += this.city ? this.city + ' ' : ''
+  fullAddress += this.state ? this.state + ' ' : ''
+  fullAddress += this.zip ? this.zip : ''
+  return fullAddress
+})
 
-export default mongoose.model(modelTypes.EMPLOYEE, employeeSchema)
+EmployeeSchema.set('toJSON', {virtuals: true})
+
+export default mongoose.model(modelTypes.EMPLOYEE, EmployeeSchema)
